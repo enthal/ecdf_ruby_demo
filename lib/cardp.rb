e549fb6643fc085309eeb9d7ca05787d69c59d09
Tim James <tjames@tjames.com>
2011-10-29 17:32:27 -0700

    move challenge descr to new README

--- lib/cardp.rb
+++ lib/cardp.rb
@@ -1,25 +1,3 @@
-# Suppose you have multiple large files (on the order of 100GBs) containing tuples of the following form: 
-# {user_id, payment_id, payment_amount, is_card_present, created_at}. 
-# Write a program to compute the empirical cumulative distribution function of the card present ratio 
-# for users who processed less than $100, and for users who processed over $100.
-#  
-# The expected output should be of the form:
-#  
-# Users who processed less than $100
-# percentile    % cp
-# 1             0
-# 2             0
-# 3             5
-# ...           ...
-# 100           100
-#  
-# and similarly for users who processed over $100.
-
-# user_id, payment_id, payment_amount, is_card_present, created_at
-
-def cp_data_row
-  [rand(10), 'x', rand(200), rand(2), 'x']
-end
 
 def parse data
 end

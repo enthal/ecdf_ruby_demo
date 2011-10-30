f6aca82a7af442f55ce9213cc31ec916abb11af9
Tim James <tjames@tjames.com>
2011-10-29 17:54:17 -0700

    scale purchase amount to avg purchases made by user, to get both buckets w good ecdf curves

--- driver.rb
+++ driver.rb
@@ -14,7 +14,11 @@ class Demo
   def data_row
     # user_id, payment_amount, is_card_present
     user_id = rand(@opts.n_users)
-    [user_id, rand(10)+10.0, (rand > @user_card_probabilities[user_id])? 1:0]
+    [
+      user_id,
+      rand(user_id/@opts.n_users.to_f*300 * (@opts.n_users/@opts.n_payments.to_f)),
+      (rand > @user_card_probabilities[user_id])? 1:0,
+    ]
   end
   
   def each
@@ -23,5 +27,5 @@ class Demo
 end
 
 
-demo = Demo.new(n_users:3000, n_payments:500000, stddev:0.1)
+demo = Demo.new(n_users:3000, n_payments:50000, stddev:0.15)
 ecdf_for_triples(30, demo).each {|vs| puts "===="; puts vs.join("\n")}

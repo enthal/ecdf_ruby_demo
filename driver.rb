56a8f81c51a078910f2b8ba513be31a58cc5480e
Tim James <tjames@tjames.com>
2011-11-02 12:58:54 -0700

    opt percentiles allows for other than 100 (for short testing output)

--- driver.rb
+++ driver.rb
@@ -23,8 +23,9 @@ class Driver
   end
 
   def ecdfs_per_spending_bucket_for_raw_card_preses raw_card_preses
+    opts = optify @opts, percentiles:100
     aggregate_card_preses = CardPres::Aggregator.new raw_card_preses
-    lt100s, gt100s = ecdfs_per_spending_bucket_for_aggregate_card_preses 100, aggregate_card_preses
+    lt100s, gt100s = ecdfs_per_spending_bucket_for_aggregate_card_preses opts.percentiles, aggregate_card_preses
   end
 
   def output_ecdfs_for_lt100s_gt100s lt100s, gt100s
